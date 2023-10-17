12da46970f842f4dfd8c70ea87c4b0dfa4e7dc7c
Harry081191 <harry081191@email.address>
2023-10-17 21:41:45 +0800

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        new DotaJFrame().init();
+        //new DotaJFrame().init();
+        System.out.println("Hello World");
     }
 }

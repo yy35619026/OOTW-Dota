63eba034faab1c213a5a2ec1e20adde20ec42752
Harry081191 <harry081191@email.address>
2023-10-21 16:24:07 +0800

    Changes

--- src/Tower/TowerCreate.java
+++ src/Tower/TowerCreate.java
@@ -33,24 +33,6 @@ public class TowerCreate {
         System.out.println("Tower 2 - Cost: " + flameTower2.getCost() + ", Damage: " + flameTower2.getDamage()
                 + ", Level: " + flameTower2.getLevel());
 
-        System.out.println("Lightning Towers:");
-        System.out.println("Tower 1 - Cost: " + lightningTower1.getCost() + ", Damage: " + lightningTower1.getDamage()
-                + ", Level: " + lightningTower1.getLevel());
-        System.out.println("Tower 2 - Cost: " + lightningTower2.getCost() + ", Damage: " + lightningTower2.getDamage()
-                + ", Level: " + lightningTower2.getLevel());
-        System.out.println("Tower 3 - Cost: " + lightningTower3.getCost() + ", Damage: " + lightningTower3.getDamage()
-                + ", Level: " + lightningTower3.getLevel());
-
-        System.out.println("Archer Towers:");
-        System.out.println("Tower 1 - Cost: " + archerTower1.getCost() + ", Damage: " + archerTower1.getDamage()
-                + ", Level: " + archerTower1.getLevel());
-        System.out.println("Tower 2 - Cost: " + archerTower2.getCost() + ", Damage: " + archerTower2.getDamage()
-                + ", Level: " + archerTower2.getLevel());
-        System.out.println("Tower 3 - Cost: " + archerTower3.getCost() + ", Damage: " + archerTower3.getDamage()
-                + ", Level: " + archerTower3.getLevel());
-        System.out.println("Tower 4 - Cost: " + archerTower4.getCost() + ", Damage: " + archerTower4.getDamage()
-                + ", Level: " + archerTower4.getLevel());
-
         JFrame frame = new JFrame("關卡一");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 450);
@@ -60,33 +42,56 @@ public class TowerCreate {
         frame.setContentPane(backgroundPanel);
         JButton imagechangeButton = getjButton("ChangeTower.png");
         JButton imagecloseButton = getjButton("CloseChange.png");
-        JButton imagearcherButton = getTowerjButton("ArcherTower.png", 0);
-        JButton imagelightningButton = getTowerjButton("LightningTower.png", 1);
+        JButton imagearcherchooseButton = getTowerChoosejButton("ArcherTower.png", 0);
+        JButton imagelightningchooseButton = getTowerChoosejButton("LightningTower.png", 1);
+        JButton imagearcherButton = getTowerjButton("ArcherTower.png");
+        JButton imagelightningButton = getTowerjButton("LightningTower.png");
         backgroundPanel.add(imagechangeButton);
 
         imagechangeButton.addActionListener(e -> {
-            backgroundPanel.add(imagearcherButton);
-            backgroundPanel.add(imagelightningButton);
+            backgroundPanel.add(imagearcherchooseButton);
+            backgroundPanel.add(imagelightningchooseButton);
             backgroundPanel.add(imagecloseButton);
             backgroundPanel.remove(imagechangeButton);
+            backgroundPanel.remove(imagearcherButton);
+            backgroundPanel.remove(imagelightningButton);
             backgroundPanel.revalidate();
             backgroundPanel.repaint();
         });
 
         imagecloseButton.addActionListener(e -> {
-            backgroundPanel.remove(imagearcherButton);
-            backgroundPanel.remove(imagelightningButton);
+            backgroundPanel.remove(imagearcherchooseButton);
+            backgroundPanel.remove(imagelightningchooseButton);
             backgroundPanel.add(imagechangeButton);
             backgroundPanel.remove(imagecloseButton);
             backgroundPanel.revalidate();
             backgroundPanel.repaint();
         });
 
-        imagearcherButton.addActionListener(e -> {
+        imagearcherchooseButton.addActionListener(e -> {
+            backgroundPanel.remove(imagearcherchooseButton);
+            backgroundPanel.remove(imagelightningchooseButton);
+            backgroundPanel.add(imagearcherButton);
+            backgroundPanel.add(imagechangeButton);
+            System.out.println("Tower 1 - Cost: " + archerTower1.getCost() + ", Damage: " + archerTower1.getDamage()
+                    + ", Level: " + archerTower1.getLevel());
+            backgroundPanel.remove(imagecloseButton);
+            backgroundPanel.revalidate();
+            backgroundPanel.repaint();
         });
 
-        imagelightningButton.addActionListener(e -> {
+        imagelightningchooseButton.addActionListener(e -> {
+            backgroundPanel.remove(imagearcherchooseButton);
+            backgroundPanel.remove(imagelightningchooseButton);
+            backgroundPanel.add(imagelightningButton);
+            backgroundPanel.add(imagechangeButton);
+            System.out.println("Tower 1 - Cost: " + lightningTower1.getCost() + ", Damage: " + lightningTower1.getDamage()
+                    + ", Level: " + lightningTower1.getLevel());
+            backgroundPanel.remove(imagecloseButton);
+            backgroundPanel.revalidate();
+            backgroundPanel.repaint();
         });
+
         frame.setVisible(true);
     }
     private JButton getjButton(String image) {
@@ -98,13 +103,26 @@ public class TowerCreate {
         imagecloseButton.setBounds(300, 150, closeW, closeH);
         return imagecloseButton;
     }
-    private JButton getTowerjButton(String image, int d) {
+    private JButton getTowerChoosejButton(String image, int d) {
         ImageIcon closeTower = new ImageIcon(image);
         Image originalcloseImage = closeTower.getImage();
         Image scaledcloseImage = originalcloseImage.getScaledInstance(closeW, closeH, Image.SCALE_SMOOTH);
         ImageIcon scaledcloseIcon = new ImageIcon(scaledcloseImage);
         JButton imagecloseButton = new JButton(scaledcloseIcon);
         imagecloseButton.setBounds(240 + d*60, 90, closeW, closeH);
+        return imagecloseButton;
+    }
+    private JButton getTowerjButton(String image) {
+        ImageIcon closeTower = new ImageIcon(image);
+        Image originalcloseImage = closeTower.getImage();
+        Image scaledcloseImage = originalcloseImage.getScaledInstance(90, 160, Image.SCALE_SMOOTH);
+        ImageIcon scaledcloseIcon = new ImageIcon(scaledcloseImage);
+        JButton imagecloseButton = new JButton(scaledcloseIcon);
+        if(image == "ArcherTower.png"){
+            imagecloseButton.setBounds(280, -10, 90, 160);
+        } else if (image == "LightningTower.png") {
+            imagecloseButton.setBounds(280, 48, 90, 102);
+        }
         return imagecloseButton;
     }
 }

5f5f334c0b97f1491e796ee1ee09bc2c62199a4a
Harry081191 <harry081191@email.address>
2023-10-31 21:29:47 +0800

    Changes

---
+++ src/BackgroundPanel.java
@@ -0,0 +1,41 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class BackgroundPanel extends JPanel {
+    private BufferedImage backgroundImage;
+    private int circleX, circleY, circleRadius;
+    private boolean circleshouldDrawCircle;
+    public void setCircle(int x, int y, int radius, boolean shouldDrawCircle) {
+        circleX = x;
+        circleY = y;
+        circleRadius = radius;
+        circleshouldDrawCircle = shouldDrawCircle;
+    }
+
+    public BackgroundPanel(String imagePath) {
+        try {
+            backgroundImage = ImageIO.read(new File(imagePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
+        System.out.println(circleX + " " + circleY);
+        if(circleshouldDrawCircle){
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setColor(new Color(255, 0, 0, 100));
+            g2d.setStroke(new BasicStroke(2.0f));
+            g2d.drawOval(circleX - circleRadius / 2 + 25, circleY - circleRadius / 2 + 25, circleRadius, circleRadius);
+        }
+    }
+}
---
+++ src/DotaJFrame.java
@@ -0,0 +1,47 @@
+import Backpaint.BackgroundPanel;
+import List.TowerSelector;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DotaJFrame extends JFrame {
+    JButton new_game, end_game;
+    public void init(){
+        //視窗
+        JFrame frame = new JFrame("第七組Tower Defence game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 450);
+
+        // 自定義面板的基礎構建
+        BackgroundPanel backgroundPanel = new BackgroundPanel("test.jpg"); // 替换成你的图像文件路径
+        backgroundPanel.setLayout(new GridBagLayout());
+        frame.setContentPane(backgroundPanel);
+
+        // GridBagConstraints物件的布局設置
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0; // X軸位置為0
+        constraints.insets = new Insets(50, 10, 10, 10);    // 設置按鈕周圍的間距
+        constraints.anchor = GridBagConstraints.CENTER;                         // 設置按鈕在Y軸上居中對齊
+        //按鈕一些屬性設置
+        Dimension buttonsize = new Dimension(150, 50);
+        Font buttonfont = new Font("Arial",Font.BOLD, 16);
+        new_game = new JButton("New Game");
+        end_game = new JButton("Exit");
+        new_game.setFont(buttonfont);
+        end_game.setFont(buttonfont);
+        new_game.setPreferredSize(buttonsize);
+        end_game.setPreferredSize(buttonsize);
+
+        constraints.gridy = 0; // Y軸位置為0
+        backgroundPanel.add(new_game, constraints);
+        constraints.gridy = 1; // Y軸位置為1
+        backgroundPanel.add(end_game, constraints);
+
+        frame.setVisible(true);
+
+        new_game.addActionListener(e -> {
+            new TowerSelector().Background();
+            SwingUtilities.getWindowAncestor(new_game).dispose();
+        });
+    }
+}

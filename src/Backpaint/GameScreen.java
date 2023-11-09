3197312a30734bae8790e0384774517061641309
Yang <s0931861498@gmail.com>
2023-11-10 00:48:51 +0800

    upDate

--- src/Backpaint/GameScreen.java
+++ src/Backpaint/GameScreen.java
@@ -55,12 +55,30 @@ public class GameScreen extends JFrame implements Runnable{
         //按鈕一些屬性設置
         Dimension buttonSize = new Dimension(150, 50);
         Font buttonFont = new Font("Arial", Font.BOLD, 16);
-        new_game = new JButton("New Game");
-        end_game = new JButton("Exit");
+        new_game = new JButton();
+        end_game = new JButton();
         new_game.setFont(buttonFont);
         end_game.setFont(buttonFont);
         new_game.setPreferredSize(buttonSize);
         end_game.setPreferredSize(buttonSize);
+        ImageIcon StartGame = new ImageIcon("./res/button/Start.png");
+        Image scaledStartImage = StartGame.getImage().getScaledInstance(150, 50, Image.SCALE_SMOOTH);
+        ImageIcon scaledStartIcon = new ImageIcon(scaledStartImage);
+        new_game.setIcon(scaledStartIcon);
+        new_game.setFocusPainted(false);
+        new_game.setOpaque(false);
+        new_game.setContentAreaFilled(false);
+        new_game.setBorderPainted(false);
+
+        ImageIcon ExitGame = new ImageIcon("./res/button/Exit.png");
+        Image scaledExitImage = ExitGame.getImage().getScaledInstance(150, 50, Image.SCALE_SMOOTH);
+        ImageIcon scaledExitIcon = new ImageIcon(scaledExitImage);
+        end_game.setIcon(scaledExitIcon);
+        end_game.setFocusPainted(false);
+        end_game.setOpaque(false);
+        end_game.setContentAreaFilled(false);
+        end_game.setBorderPainted(false);
+
 
         constraints.gridy = 0; // Y軸位置為0
         backgroundPanel.add(new_game, constraints);

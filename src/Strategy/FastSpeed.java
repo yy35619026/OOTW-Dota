17371e3ac2268cbd8e542e00633618df50fbed38
李戈玉靈 <abc103400@gmail.com>
2023-11-10 01:43:31 +0800

    strategy

---
+++ src/Strategy/FastSpeed.java
@@ -0,0 +1,13 @@
+package Strategy;
+
+public class FastSpeed implements SpeedStrategy{
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void updatePosition(float x, float y) {
+
+    }
+}
---
+++ src/Strategy/SlowSpeed.java
@@ -0,0 +1,13 @@
+package Strategy;
+
+public class SlowSpeed implements SpeedStrategy{
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void updatePosition(float x, float y) {
+
+    }
+}
